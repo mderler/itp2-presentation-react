926e373bb716f5e4735160e36f92d8df43af9546
mderler <5matthiasderler5@gmail.com>
2022-05-15 15:13:15 +0200

    counter

---
+++ react-demo/src/App.js
@@ -0,0 +1,12 @@
+import Counter from "./components/counter";
+
+function App() {
+  return (
+    <>
+      <h1>Hello, World</h1>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
---
+++ react-demo/src/components/counter.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+
+  render() {
+    return (
+      <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+        {this.state.count}
+      </button>
+    );
+  }
+}
+
+export default Counter;
